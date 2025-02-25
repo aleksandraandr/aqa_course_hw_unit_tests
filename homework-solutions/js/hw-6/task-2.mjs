f43872555d894cce20c57adc3f2a3032a64f48a6
aleksandraandr <ponytail1986@gmail.com>
2025-02-25 13:53:57 +0100

    Addet task-1.js, task-2.js, task-3.js

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -13,7 +13,54 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+
 let resultUnique;
 let resultNull;
+
+let competitorPizzasLower = [];
+for (let pizza of competitorPizzas) {
+  competitorPizzasLower.push(pizza.toLowerCase());
+}
+
+let resultUniqueT1 = [];
+for (let myPizza of myPizzasT1) {
+  let isUnique = true;
+  for (let competitorPizza of competitorPizzasLower) {
+    if (myPizza.toLowerCase() === competitorPizza) {
+      isUnique = false;
+    }
+  } 
+  if (isUnique) {
+    resultUniqueT1.push(myPizza);
+  }
+}
+
+let resultUniqueT2 = [];
+for (let myPizza of myPizzasT2) {
+  let isUnique = true;
+  for (let competitorPizza of competitorPizzasLower) {
+    if (myPizza.toLowerCase() === competitorPizza) {
+      isUnique = false;
+    }
+  }
+  if (isUnique) {
+    resultUniqueT2.push(myPizza);
+  }
+}
+
+if (resultUniqueT1.length > 0) {
+  resultUnique = resultUniqueT1;
+  resultNull = null;
+} else if (resultUniqueT2.length > 0) {
+  resultUnique = resultUniqueT2;
+  resultNull = null;
+} else {
+  resultUnique = null;
+  resultNull = null;
+}
+
 
 export { resultNull, resultUnique };
